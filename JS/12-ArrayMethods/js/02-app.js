e2a750590701008c80463797d36c102c99623b45
Jilver <jilver.practicando@gmail.com>
2021-04-06 18:02:38 -0500

    practicando metodos de arreglos

--- JS/12-ArrayMethods/js/02-app.js
+++ JS/12-ArrayMethods/js/02-app.js
@@ -1,16 +1,51 @@
+frutas= ['manzana', 'durazno', 'pera']
 productos=[
     {nombre:'lavadora', precio:30},
     {nombre:'plancha', precio:70},
-    {nombre:'tv', precio:20}
+    {nombre:'tv', precio:30},
+    {nombre:'tv', precio:60}
 ]
 
-let resultado = productos.some((element)=>{
-   return element.nombre === 'plancha'
-})
+productos2=[
+  
+    {nombre:'rario', precio:30},
+    {nombre:'licuadora', precio:60}
+]
+
+// includes
+resultado = frutas.includes('manzana')
 console.log(resultado)
 
-
-let resultado2 = productos.every( producto => producto === 'tv')
+// some 
+let resultado2 = productos.some(producto => producto.nombre === 'lavadora') //almenos un elemento tien que cumplir
 console.log(resultado2)
 
-let resultado3=
+let resultado3=productos.every(producto => producto.nombre === 'lavadora')
+console.log(resultado3)
+
+//findIndex
+let resultado4=productos.findIndex(producto => producto.nombre === 'tv') // te bota el indice donde lo encuentra sino lo encuentra le coloca un -1
+console.log(resultado4)
+
+//reduce
+let resultado5=productos.reduce((total, producto) => total + producto.precio, 0 )
+console.log(resultado5)
+
+// filter
+let resultado6=productos.filter(producto => producto.nombre !== 'tv')
+console.log(resultado6)
+
+//find
+let resultado7= productos.find(producto => producto.nombre = 'tv')
+console.log(resultado7)
+
+
+//concat
+
+let resultado8 = productos.concat(frutas,productos2)
+console.log(resultado8)
+
+//Spread Operator
+
+let resultado9= {productos }
+console.log(resultado9)
